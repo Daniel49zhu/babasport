1160d7fb5b4fbdfb4be9073ee7c6ddd49c2721ef
Daniel49zhu <zhujiacheng94@163.com>
2019-05-05 21:28:05 +0800

    更新

--- src/main/java/com/zjc/core/dao/TestTbDao.java
+++
@@ -1,9 +0,0 @@
-package com.zjc.core.dao;
-
-import com.zjc.core.bean.TestTb;
-import org.apache.ibatis.annotations.Insert;
-
-public interface TestTbDao {
-    @Insert("insert into test_tb (name,birthday) values (#{name},#{birthday})")
-    void addTestTb(TestTb testTb);
-}
--- src/main/java/com/zjc/core/service/TestTbService.java
+++
@@ -1,7 +0,0 @@
-package com.zjc.core.service;
-
-import com.zjc.core.bean.TestTb;
-
-public interface TestTbService {
-    void addTestTb(TestTb testTb);
-}
