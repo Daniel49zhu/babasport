0f7c3d2212af04d040285d189ff84caeda7c53bb
Daniel49zhu <zhujiacheng94@163.com>
2019-05-12 16:24:27 +0800

    商品分页

--- babasport-console/src/main/java/com/zjc/core/controller/BrandController.java
+++ babasport-console/src/main/java/com/zjc/core/controller/BrandController.java
@@ -1,14 +1,28 @@
 package com.zjc.core.controller;
 
+import cn.itcast.common.page.Pagination;
+import com.zjc.core.service.product.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BrandController {
+    @Autowired
+    private BrandService brandService;
     //查询
     @RequestMapping(value = "/brand/list.do")
     public String list(String name, Integer isDisplay, Integer pageNo, Model model) {
+
+        Pagination pagination = brandService.selectPaginationByQuery(name, isDisplay, pageNo);
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("name", name);
+        if(null != isDisplay){
+            model.addAttribute("isDisplay", isDisplay);
+        }else{
+            model.addAttribute("isDisplay", 1);
+        }
 
         return "brand/list";
     }
